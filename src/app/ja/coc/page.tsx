0ff495165fd828e08b04d7b529eb75ec2a6fbaba
k-nakamura <k-nakamura@users.noreply.github.com>
2026-06-02 21:03:45 +0900

    Revert "Feature/cfp close community open"

--- src/app/ja/coc/page.tsx
+++
@@ -1,8 +0,0 @@
-import MarkdownPage from "@/app/_components/templates/MarkdownPage";
-import {renderMarkdown} from "@/app/_lib/markdown";
-import "@/app/globals.css";
-
-export default async function CocJa() {
-  const html = await renderMarkdown('coc/ja.md');
-  return <MarkdownPage html={html}/>;
-}
